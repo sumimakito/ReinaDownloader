3a5a454d9a1363cfbf9d3a5cb79fef2fa8ce3d8f
SumiMakito <sumimakito@hotmail.com>
2014-11-09 13:54:27 +0800

    initial

---
+++ src/sumimakito/android/reina/lib/downloader/DBOpenHelper.java
@@ -0,0 +1,25 @@
+package sumimakito.android.reina.lib.downloader;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+	private static final String DBNAME = "rndownload.db";
+	private static final int VERSION = 1;
+
+	public DBOpenHelper(Context context) {
+		super(context, DBNAME, null, VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE IF NOT EXISTS filedownlog (id integer primary key autoincrement, downpath varchar(100), threadid INTEGER, downlength INTEGER)");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS filedownlog");
+		onCreate(db);
+	}
+}
