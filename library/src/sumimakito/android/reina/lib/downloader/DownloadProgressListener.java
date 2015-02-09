92c364c912afa5b86ea167ad86f6472d6a41cfff
SumiMakito <sumimakito@hotmail.com>
2015-02-09 18:12:03 +0800

    update to v1.1.0

--- library/src/sumimakito/android/reina/lib/downloader/DownloadProgressListener.java
+++
@@ -1,8 +0,0 @@
-package sumimakito.android.reina.lib.downloader;
-
-public interface DownloadProgressListener {  
-    public void onDownloadProgress(int size);  
-	public void onDownloadError(Exception e);
-	public void onDownloadFinish();
-	public void onDownloadStart(int size);
-}  
